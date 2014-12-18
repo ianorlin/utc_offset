79c6d7e0f983753ba2d75327f136757292fefbba
Brendan Perrine <walterorlin@gmail.com>
2014-12-18 12:27:37 -0800

    Add support for calendar date different than utc
    Added support for the new year as first case so then I could easily use the day number in the second case
    Made a lot of if statements but is good in best case if you are on same day as utc.

--- utc_offset.sh
+++ utc_offset.sh
@@ -2,7 +2,17 @@
 #Copyright 2014 Brendan Perrine
 # This is a simple bash script that shows the offset to UTC from your current time zone 
 
-
-
-echo " $(date  +%H) - $(date  -u +%H) "|bc
-
+ 
+#note that can't have same day on different years as are not that many time zones  
+if [ $(date +%j)  -eq $(date -u +%j) ] ; then 
+  echo " $(date  +%H) - $(date  -u +%H) "|bc
+#new years first because of otheruwise I can't use day number accuratly
+elif [ $(date +%Y) -gt $(date -u +%Y ) ]; then 
+  echo " [[$( date +%H )+24 ]]  - $(date -u +%H)"|bc
+elif [ $(date +%Y) -lt $(date -u +%Y) ] ;then
+  echo " $(date +%H ) -  [[$( date +%H )+ 24 ]] "|bc
+elif [ $(date +%j ) -gt $( date -u +%j )] ; then 
+ echo " [[$(date +%H +24) ]] - $(date -u +%H) "|bc 
+else 
+ echo "$(date +%H ) - [[$( date -u +%h) + 24 ]]  "|bc
+fi 
