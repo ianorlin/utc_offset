88af80ca73d18b305959cec9f119979cb2603369
Brendan Perrine <walterorlin@gmail.com>
2014-12-18 17:09:15 -0800

    Fix different days  Fix the different days and also make syntax simpler by just distributing out the the adding or subtracting of 24 instead of using 2 stage bash math that caused syntax error and now do calculation of offset in bc.

--- utc_offset.sh
+++ utc_offset.sh
@@ -8,11 +8,12 @@ if [ $(date +%j)  -eq $(date -u +%j) ] ; then
   echo " $(date  +%H) - $(date  -u +%H) "|bc
 #new years first because of otheruwise I can't use day number accuratly
 elif [ $(date +%Y) -gt $(date -u +%Y ) ]; then 
-  echo " [[$( date +%H )+24 ]]  - $(date -u +%H)"|bc
-elif [ $(date +%Y) -lt $(date -u +%Y) ] ;then
-  echo " $(date +%H ) -  [[$( date +%H )+ 24 ]] "|bc
-elif [ $(date +%j ) -gt $( date -u +%j )] ; then 
- echo " [[$(date +%H +24) ]] - $(date -u +%H) "|bc 
+  echo " $( date +%H ) + 24   - $(date -u +%H)"|bc
+elif [ $(date +%Y) -lt $(date -u +%Y) ] ; then
+  echo " $(date +%H ) -  $( date +%H ) - 24 "|bc
+elif [ $(date +%j ) -gt $( date -u +%j ) ] ; then 
+ echo " $(date +%H ) - $(date -u +%H) +24 "|bc 
 else 
- echo "$(date +%H ) - [[$( date -u +%h) + 24 ]]  "|bc
+ echo " $(date +%H ) - $( date -u +%H) -24 "|bc
+
 fi 
